7f7b0822313f6c0cf5412f7e820d006b9f1e2a3f
Alan W <awangc31s@gmail.com>
2025-03-12 18:04:40 -0400

    SDLKFJ

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -21,9 +21,9 @@ const LeaderboardCell = () => {
   }, [socket]);
 
   return (
-    <div>
+    <div className="overflow-y-auto h-full">
       <table className="ranking-table table-auto border-collapse w-full">
-        <thead className="outline outline-1 outline-offset-0 outline-red sticky top-0">
+        <thead className="outline outline-1 outline-offset-0 sticky top-0">
           <tr className="bg-black">
             <th className="text-left px-4 py-2">Ranking</th>
             <th className="text-left px-4 py-2">Username</th>
@@ -32,7 +32,7 @@ const LeaderboardCell = () => {
         </thead>
         <tbody className="border-separate">
           {rankings.map((player_info, index) => (
-            <tr key={index} className="border-b border-gray-300">
+            <tr key={index} className={`border-b border-gray-300`}>
               <td className="px-4 h-10">{index + 1}</td>
               <td className="px-4 h-10">{player_info[0]}</td>
               <td className="px-4 h-10">{player_info[1]}</td>
